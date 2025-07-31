1fc845b6fb176d6ce8eab45781c8cf9b797c85ed
Julio Soto <106524908+jdjuliosoto@users.noreply.github.com>
2025-07-31 04:45:02 +0200

    Update Diff_abun_G1vsG2.R

--- Diff_abun_G1vsG2.R
+++ Diff_abun_G1vsG2.R
@@ -1,5 +1,5 @@
-# Differential abundance taxa, metabolic pathways and virulence factors across two groups of samples described by PCA-GMM, Bray-Curtis PoCA, PCA of the VST abundance data and Virulence Factors.
- 
+# Differential Abundance of Taxa, Metabolic Pathways, and Virulence Factors Across Two Sample Groups Identified by Multivariate Clustering Methods"
+# Including PCA-GMM, Bray-Curtis PoCA, and PCA of VST-Transformed Abundance Data 
 
 ```R
 # CRAN libraries
